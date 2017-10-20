1b33a990999509d1538f9f6286a32b786f8010d9
Álvaro Peralta <alvaroperalta@MacBook-Air-de-Alvaro.local>
2017-10-20 15:46:07 +0200

    Changes in endpoint of rabbit

--- src/main/scala/poc/camel/CamelConsumer.scala
+++ src/main/scala/poc/camel/CamelConsumer.scala
@@ -4,7 +4,7 @@ import akka.camel.Consumer
 import akka.stream.actor.ActorPublisher
 
 class CamelConsumer extends Consumer with ActorPublisher[String] {
-  def endpointUri = "rabbitmq://http://127.0.0.1:8081/consumerExchange?username=guest&password=guest&autoDelete=false&routingKey=camel&queue=cola1"
+  def endpointUri = "rabbitmq://127.0.0.1:8081/consumerExchange?username=guest&password=guest&autoDelete=false&routingKey=camel&queue=cola1"
 
   import akka.stream.actor.ActorPublisherMessage._
 
