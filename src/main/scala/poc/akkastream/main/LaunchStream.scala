621e649987b6cca8bcec157a039aabdcf5424c34
sucius <alvaro.pozo.cuenca@gmail.com>
2017-10-26 17:16:49 +0200

    added thtoughput

--- src/main/scala/poc/akkastream/main/LaunchStream.scala
+++ src/main/scala/poc/akkastream/main/LaunchStream.scala
@@ -1,7 +1,7 @@
 package poc.akkastream.main
 
 import akka.NotUsed
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.stream.actor.ActorPublisher
 import akka.stream.scaladsl.{Flow, GraphDSL, RunnableGraph, Sink, Source}
 import akka.stream.{ActorMaterializer, ClosedShape, OverflowStrategy}
@@ -14,32 +14,43 @@ object LaunchStream extends App {
 
   implicit val system = ActorSystem("some-system")
   implicit val materializer = ActorMaterializer()
-  val consumerActor = system.actorOf(Props[CamelConsumer])
-
-  val source: Source[String, NotUsed] = {
-    val publisher: Publisher[String] = ActorPublisher(consumerActor)
-    Source.fromPublisher(publisher).buffer(5000, OverflowStrategy.backpressure)
-  }
-  val sink =
-    Sink.actorRefWithAck[String](system.actorOf(Props(new CamelSubscriber(consumerActor.path))), INITMESSAGE, ACK, ONCOMPLETE, th => th.getMessage)
 
   publishInRabbit
 
-  val graphScenario = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
-    import GraphDSL.Implicits._
-    val in = source
-    val out = sink
+  //Scenario with 1000 buffered
+  val camelConsumerActor = consumerCamelActor
+  graphCamelScenario(sourceForCamel(camelConsumerActor), sinkForCamel(consumerCamelActor), 1000)
 
-    val f1 = Flow[String].map(_.toString)
-    val f2 = Flow[String].map(_ + " Flow2")
 
-    in ~> f1 ~> f2 ~> out
-    ClosedShape
-  }).run()
+  /** CAMEL AKKA STREAMING **/
+
+  private def graphCamelScenario(source: Source[String, NotUsed], sink: Sink[String, NotUsed], buffer: Int) =
+    RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+      import GraphDSL.Implicits._
+      val in = source.buffer(buffer, OverflowStrategy.backpressure)
+      val out = sink
+
+      val f1 = Flow[String].map(_.toString)
+      val f2 = Flow[String].map(_ + " Flow2")
+
+      in ~> f1 ~> f2 ~> out
+      ClosedShape
+    })
 
 
   private def publishInRabbit = {
     val publish: PublisherBase = PublisherMain.apply
-    publish.basicPublish("localhost", 8081, "hola vengo de rabbit")("consumerExchange", "cola1", "camel", 50000)
+    publish.basicPublish("localhost", 8081, "hola vengo de rabbit")("consumerExchange", "cola1", "camel", 5000)
   }
+
+  private def consumerCamelActor = system.actorOf(Props[CamelConsumer])
+
+  private def sourceForCamel(consumer: ActorRef): Source[String, NotUsed] = {
+    val publisher: Publisher[String] = ActorPublisher(consumer)
+    Source.fromPublisher(publisher)
+  }
+
+  private def sinkForCamel(consumer: ActorRef) =
+    Sink.actorRefWithAck[String](system.actorOf(Props(new CamelSubscriber(consumer.path))),
+      INITMESSAGE, ACK, ONCOMPLETE, th => th.getMessage)
 }
